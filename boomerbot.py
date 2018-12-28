0089214fcd55f9078640920b1450b37652c302aa
LeeMacCloudy <45078531+LeeMacCloudy@users.noreply.github.com>
2018-12-29 05:15:09 +0800

    Update boomerbot.py

--- boomerbot.py
+++ boomerbot.py
@@ -17,24 +17,26 @@ async def on_ready():
 
 @bot.command(pass_context=True)
 async def ping(ctx):
-    await bot.say(":ping_pong: ping!! xSSS")
-    print ("user has pinged")
-
-@bot.command(pass_context=True)
-async def info(ctx, user: discord.Member):
-    await bot.say("The users name is: {}".format(user.name))
-    await bot.say("The users ID is: {}".format(user.id))
-    await bot.say("The users status is: {}".format(user.status))
-    await bot.say("The users highest role is: {}".format(user.top_role))
-    await bot.say("The user joined at: {}".format(user.joined_at))
+    embed = discord.Embed(
+           title = "Information",
+           description = "Pong! 555ms"
+           colour = discord.Colour.blue()
+           print ("user has pinged")
 
 @bot.command(pass_context=True)
 async def kick(ctx, user: discord.Member):
     if ctx.message.author.server_permissions.kick_members:
-       await bot.say(":boot: I kicked {}.".format(user.name))
+       embed = discord.Embed(
+           title = "Information",
+           description="I kicked {}.".format(user.name),
+           colour = discord.Colour.blue()
        await bot.kick(user)
     else:
-       await bot.say("Sorry, You need `Kick members` To use this command!")
+       embed = discord.Embed(
+           title = "Oops!",
+           description ="You need KICK MEMBERS permission to do that`
+           KICK MEMBERS permission to do that!"
+           colour = discord.Colour.red()
 
 
 #THIS MUST BE THIS WAY
