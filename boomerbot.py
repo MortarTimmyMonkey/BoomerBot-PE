dc278f4417580d8198d1bb4f8efa4ecbdc29707d
LeeMacCloudy <45078531+LeeMacCloudy@users.noreply.github.com>
2018-12-29 17:16:48 +0800

    Update boomerbot.py

--- boomerbot.py
+++ boomerbot.py
@@ -22,14 +22,16 @@ async def ping(ctx):
            description = "Pong! 555ms"
            colour = discord.Colour.blue()
            print ("user has pinged")
+           )
 
 @bot.command(pass_context=True)
 async def kick(ctx, user: discord.Member):
     if ctx.message.author.server_permissions.kick_members:
-       embed = discord.Embed(
+       embed = discord .Embed(
            title = "Information",
            description="I kicked {}.".format(user.name),
            colour = discord.Colour.blue()
+           )
        await bot.kick(user)
     else:
        embed = discord.Embed(
@@ -37,6 +39,7 @@ async def kick(ctx, user: discord.Member):
            description ="You need KICK MEMBERS permission to do that`
            KICK MEMBERS permission to do that!"
            colour = discord.Colour.red()
+           )
 
 
 #THIS MUST BE THIS WAY
